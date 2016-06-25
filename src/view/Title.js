b4cda0971392493b3cd79e1b9cd77a8fbb471e64
Geoffrey Bachelet <geoffrey.bachelet@gmail.com>
2016-06-25 18:44:18 +0200

    Add an oniric music

--- src/view/Title.js
+++ src/view/Title.js
@@ -1,21 +1,72 @@
 class Title {
     constructor(thrust) {
         this.thrust = thrust;
-        this.onStart = this.onStart.bind(this);
+        this.onKeyDown = this.onKeyDown.bind(this);
         this.overlay = document.getElementById('title');
+        this.audioContext = new AudioContext();
+        this.ostBuffer;
+        this.audioSource;
+        this.audioPlaying = false;
+        this.loadOst = this.loadOst.bind(this);
+        this.playOst = this.playOst.bind(this);
 
-        document.body.addEventListener('keydown', this.onStart)
+        // this.loadOst(this.playOst);
+
+        document.body.addEventListener('keydown', this.onKeyDown)
     }
 
-    onStart(event) {
-        if (event.keyCode == 32) {
-            if (this.thrust.started) {
-                this.overlay.style.display = 'block';
-                this.thrust.stop();
-            } else {
-                this.overlay.style.display = 'none';
-                this.thrust.start();
-            }
+    loadOst(done) {
+        let request = new XMLHttpRequest();
+        request.open('GET', '/ost/title.ogg');
+        request.responseType = 'arraybuffer';
+
+        request.onload = function() {
+            this.audioContext.decodeAudioData(request.response, function(buffer) {
+                this.ostBuffer = buffer;
+                done();
+            }.bind(this));
+        }.bind(this);
+
+        request.send();
+    }
+
+    playOst() {
+        this.audioSource = this.audioContext.createBufferSource();
+        this.audioSource.buffer = this.ostBuffer;
+        this.audioSource.connect(this.audioContext.destination);
+        this.audioSource.start(0);
+
+        this.audioPlaying = true;
+    }
+
+    onKeyDown(event) {
+        switch (event.keyCode) {
+            case 32:
+                this.toggleState();
+                break;
+            case 84:
+                this.toggleSound();
+                break;
+        }
+    }
+
+    toggleSound() {
+        if (this.audioPlaying) {
+            this.audioSource.disconnect();
+        } else {
+            this.audioSource.connect(this.audioContext.destination);
+        }
+
+        this.audioPlaying = !this.audioPlaying;
+    }
+
+    toggleState() {
+        if (this.thrust.started) {
+            this.overlay.style.display = 'block';
+            this.thrust.stop();
+        } else {
+            this.overlay.style.display = 'none';
+            this.thrust.start();
         }
     }
 }
