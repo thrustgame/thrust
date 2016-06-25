e23a66d2c2ef1f36f11a141463db050fbe88d32f
Thomas Jarrand <thomas.jarrand@gmail.com>
2016-06-25 19:57:29 +0200

    Nice avatars

--- src/view/Avatar.js
+++ src/view/Avatar.js
@@ -5,25 +5,30 @@ class Avatar {
         this.player = player;
         this.radius = 50;
 
-        this.idle = Avatar.createLozange('#FFFB52', direction);
-        this.thrust = Avatar.createLozange('#BCBA3C', direction);
+        this.idle = Avatar.createLozange('#FFFB52', '#e2df48', direction);
+        this.thrust = Avatar.createLozange('#fffa1c', '#eae61b', direction);
     }
 
-    static createLozange(color, direction) {
+    static createLozange(color, colorDark, direction) {
         const canvas = new Canvas(100, 100);
         const context = canvas.element.getContext('2d');
-
-        canvas.setFill(color);
-        context.beginPath();
 
         if (direction == 'rtl') {
             canvas.reverse();
         }
 
+        canvas.setFill(color);
+        context.beginPath();
+        context.moveTo(25,50);
+        context.lineTo(75,25);
+        context.lineTo(100,50);
+        context.fill();
+
+        canvas.setFill(colorDark);
+        context.beginPath();
         context.moveTo(25,50);
         context.lineTo(75,75);
         context.lineTo(100,50);
-        context.lineTo(75,25);
         context.fill();
 
         if (direction == 'rtl') {
