b0be0d4751b95658791706eab1737fcce6358b26
Thomas Jarrand <thomas.jarrand@gmail.com>
2016-06-26 01:01:11 +0200

    Fixed first room

--- src/engine/Corridor.js
+++ src/engine/Corridor.js
@@ -9,15 +9,19 @@ class Corridor {
         this.rooms = [];
         this.distance = 0;
 
+        this.addRoom(Corridor.roomSize * 2);
+
         for (var i = 0; i < length; i++) {
             this.addRoom();
         }
+
+        this.addRoom(Corridor.roomSize * 2);
     }
 
-    addRoom() {
+    addRoom(forceSize = 0) {
         const length = this.rooms.length;
         const start = length ? this.rooms[length - 1].end : 0;
-        const size = Math.round(Corridor.roomSize * (1 + (Math.random() - 0.5)));
+        const size = forceSize ? forceSize : Math.round(Corridor.roomSize * (1 + (Math.random() - 0.3)));
 
         this.rooms.push(new Room(length, start, start + size));
         this.distance += size;
