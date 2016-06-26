27df7b1d1092a71b3498091377582bb2325a9f65
Thomas Jarrand <thomas.jarrand@gmail.com>
2016-06-26 22:37:23 +0200

    Added a third color

--- src/engine/Room.js
+++ src/engine/Room.js
@@ -1,12 +1,21 @@
 class Room {
+
+    static colors = [
+        ['#D64F9E', '#9C4377'],
+        ['#5BD3F0', '#50A2B6'],
+        ['#5B52B8', '#45407E'],
+    ];
+
     constructor(id, start, end) {
+        const colors = Room.colors[id % Room.colors.length];
+
         this.id = id;
         this.start = start;
         this.end = end;
         this.size = this.end - this.start;
         this.wall = this.end - (this.size * 0.1);
-        this.wallColor = this.id % 2 === 0 ? '#BC468B' : '#47A6BC';
-        this.color = this.id % 2 === 0 ? '#D64F9E' : '#5BD3F0';
+        this.color = colors[0];
+        this.wallColor = colors[1];
         this.view = null;
         this.mirror = null;
     }
