4318c631dd15302d9ad126c8f1c77c5ef57c20cf
Thomas Jarrand <thomas.jarrand@gmail.com>
2016-06-26 23:19:05 +0200

    Added useless but pretty shadow

--- src/view/Camera.js
+++ src/view/Camera.js
@@ -14,8 +14,18 @@ class Camera {
         const size = this.avatar.getSize();
         const x = Math.round(this.centerX - size / 2);
         const y = Math.round(this.centerY - size / 2);
+        const shadow = Avatar.radius * 0.1;
         const shake = this.avatar.getShake();
         const { start, end } = this.getViewPort();
+        let aesize;
+        let aex;
+        let aey;
+
+        if (alterEgo) {
+            aesize = alterEgo.getSize();
+            aex = this.getAlterEgoPosition(x, difference);
+            aey = Math.round(this.centerY - aesize / 2);
+        }
 
         for (let  i = this.map.corridor.rooms.length - 1; i >= 0; i--) {
             let room = this.map.corridor.rooms[i];
@@ -24,12 +34,15 @@ class Camera {
             }
         }
 
-        this.canvas.drawImage(this.avatar.draw(), x, y + shake, size, size);
+        this.canvas.drawImage(this.avatar.drawShadow(), x + shake, y + shadow, size, size);
 
         if (alterEgo) {
-            const aesize = alterEgo.getSize();
-            const aex = this.getAlterEgoPosition(x, difference);
-            const aey = Math.round(this.centerY - aesize / 2);
+            this.canvas.drawImage(alterEgo.drawShadow(), aex, aey + shadow, aesize, aesize);
+        }
+
+        this.canvas.drawImage(this.avatar.draw(), x + shake, y, size, size);
+
+        if (alterEgo) {
             this.canvas.drawImage(alterEgo.draw(), aex, aey, aesize, aesize);
         }
     }
